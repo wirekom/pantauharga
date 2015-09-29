8460c536ea5231e7c16dd08d9a4986a30bea08b8
Widodo Pangestu <wid.pangestu@gmail.com>
2015-09-29 15:52:14 +0700

    /

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -12,8 +12,8 @@ class UrlMappings {
         "/api/comodities"(resources: "comodity")
         "/api/comodityInputs"(resources: "comodityInput")
         "/api/comodityTypes"(resources: "comodityType")
-        "/"(controller: 'comodityInput', action: 'map')
         */
+        "/"(controller: 'comodityInput', action: 'map')
         "500"(view: '/error')
         "404"(view: '/error')
         "403"(view: '/error')
