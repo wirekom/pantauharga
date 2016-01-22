72e252de899d7611a89d8e994b12c165e6d47044
Yohanes Gultom <yohanes.gultom@gmail.com>
2016-01-22 09:17:24 +0700

    remove create user/role in input & inputRequest

--- grails-app/controllers/com/pantau/core/ApiController.groovy
+++ grails-app/controllers/com/pantau/core/ApiController.groovy
@@ -83,77 +83,13 @@ class ApiController {
             }
             return
         }
-        def member = AuthUser.findByNohp(instanceCommodity.nohp) ?: new AuthUser(
-                username: instanceCommodity.nohp,
-                password: instanceCommodity.nohp,
-                nohp: instanceCommodity.nohp,
-                enabled: true).save(flush: true)
-        println 'user ' + member.username
-
-        def roleUser = AuthRole.findByAuthority('ROLE_USER')
-        if (!AuthUserAuthRole.exists(member.id, roleUser.id)) {
-            AuthUserAuthRole.create member, roleUser, true
-        }
-
-       // def last = ComodityInput.list([max: 1, sort: 'dateCreated', order: 'asc'])
         Double dt = 0
-        //if (!last.isEmpty()) {
-        //    dt = instanceCommodity.harga - last.first().price
-       // }
-
-        //def (lat, lng) = instanceCommodity.geolocation.tokenize(',')
-       /// BigDataRequestModel big = new BigDataRequestModel()
-       // def jsonSlurper = new JsonSlurper()
-       // // def json = jsonSlurper.parseText(big.getNearby(Double.toString(instanceCommodity.lat), Double.toString(instanceCommodity.lng), '10'))
-       // def json = big.getNearby(Double.toString(instanceCommodity.lat), Double.toString(instanceCommodity.lng), '10')
-       // def search
-       // def apa = json.result
-
-      //  println "sjon" + json
-      /*  for (def ret : json.result) {
-            println "ret" + ret
-            //  def apa = jsonSlurper.parseText(ret.toString())
-            // println apa
-
-            if (ret.masterclass == "Commercial") {
-                if (Double.parseDouble(ret?.latitude?.toString()))
-                    search = ret
-                break
-            }
-        }*/
-       // println "search" + search.province
-      //  Region prop = Region.findByName(search.province)
-        /*
-        if (prop?.name == null) {
-            prop = new Region(name: search.province, geolocation: search.latitude + "," + search.longitude).save(flush: true)
-        }
-        println prop
-        Region district = Region.findByName(search.district)
-        if (district?.name == null) {
-            district = new Region(name: search.district, geolocation: search.latitude + "," + search.longitude)
-            district.setParent(prop)
-            district.save(flush: true)
-        }
-
-        Location loc = Location.findByName(search.name) ?: new Location(
-                name: search.name,
-                lat: search.latitude,
-                lng: search.longitude
-        ).save(flush: true)
-        if (instanceCommodity.quantity == 0) {
-            instanceCommodity.lat = Double.parseDouble(search.latitude)
-            instanceCommodity.lng = Double.parseDouble(search.longitude)
-
-        }*/
-        //akal2an
-
         Region district = Region.find("FROM Region ORDER BY id")
         Integer type = 0
         if (instanceCommodity.quantity > 0) {
             type = 1
-
         }
-
+        def member = AuthUser.findByNohp(instanceCommodity.nohp)
         def com = new ComodityInput(user: member, comodityName: comodity, price: instanceCommodity.harga, lat: instanceCommodity.lat, lng: instanceCommodity.lng, amount: instanceCommodity.quantity, type:type, delta: dt, region: district)
         if (!com.save(flush: true)) {
             println 'error ' + com.errors.allErrors.join(' \n')
@@ -223,17 +159,6 @@ class ApiController {
             }
             return
         }
-        def member = AuthUser.findByNohp(instanceCommodity.nohp) ?: new AuthUser(
-                username: instanceCommodity.nohp,
-                password: instanceCommodity.nohp,
-                nohp: instanceCommodity.nohp,
-                enabled: true).save(flush: true)
-        println 'user ' + member.username
-
-        def roleUser = AuthRole.findByAuthority('ROLE_USER')
-        if (!AuthUserAuthRole.exists(member.id, roleUser.id)) {
-            AuthUserAuthRole.create member, roleUser, true
-        }
 
         // def last = ComodityInput.list([max: 1, sort: 'dateCreated', order: 'asc'])
         Double dt = 0
@@ -241,7 +166,7 @@ class ApiController {
         Region district = Region.find("FROM Region ORDER BY id")
         Integer type = 2
 
-
+        def member = AuthUser.findByNohp(instanceCommodity.nohp)
         def com = new ComodityInput(user: member, comodityName: comodity, price: instanceCommodity.harga, lat: instanceCommodity.lat, lng: instanceCommodity.lng, amount: instanceCommodity.quantity, type:type, delta: dt, region: district)
         if (!com.save(flush: true)) {
             println 'error ' + com.errors.allErrors.join(' \n')
